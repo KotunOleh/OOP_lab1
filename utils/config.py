39328158badda2e16c72003fe484503ecf0fbb24
Oleh Kotun <ijachok20@gmail.com>
2025-10-26 23:31:47 +0200

    utils dir

---
+++ utils/config.py
@@ -0,0 +1,7 @@
+SCOPES = ['https://www.googleapis.com/auth/drive']
+APP_NAME = "KotunSpreadSheeter"
+DEFAULT_SHEET_NAME = "Sheet1"
+DEFAULT_ROWS = 10
+DEFAULT_COLS = 5
+TOKEN_FILE = 'token.json'
+CREDENTIALS_FILE = 'credentials.json'
